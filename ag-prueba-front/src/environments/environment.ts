af7991b8682161ee66c46134186037d03ef9e513
Felipe Santos <hydfelipe@gmail.com>
2025-12-04 15:23:00 +0100

    Eliminada mala configuracion innecesaria para front

--- ag-prueba-front/src/environments/environment.ts
+++ ag-prueba-front/src/environments/environment.ts
@@ -1,17 +1,15 @@
-import { globalConfig } from './environment.global';
-
 export const environment = {
-  production: false,
-  authorized_uri: `${globalConfig.authServerUrl}/oauth2/authorize?`,
-  client_id: globalConfig.clientId,
-  redirect_uri: globalConfig.redirectUri,
-  scope: 'openid profile',
-  response_type: 'code',
-  response_mode: 'form_post',
-  code_challenge_method: 'S256',
-  token_url: `${globalConfig.authServerUrl}/oauth2/token`,
-  grant_type: 'authorization_code',
-  resource_url: `${globalConfig.apiBaseUrl}/resource/`,
-  logout_url: `${globalConfig.authServerUrl}/logout`,
-  secret_pkce: process.env.SECRET_PKCE || 'secret'
+    production: false,
+    authorized_uri: 'http://localhost:9000/oauth2/authorize?',
+    client_id: 'client',
+    redirect_uri: 'http://127.0.0.1:4200/authorized',
+    scope: 'openid profile',
+    response_type: 'code',
+    response_mode: 'form_post',
+    code_challenge_method: 'S256',
+    token_url: 'http://localhost:9000/oauth2/token',
+    grant_type: 'authorization_code',
+    resource_url: 'http://localhost:8080/resource/',
+    logout_url: 'http://localhost:9000/logout',
+    secret_pkce: 'secret'
 };
