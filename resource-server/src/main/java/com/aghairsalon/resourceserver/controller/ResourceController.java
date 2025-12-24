32c9d71e2c4671830853f05c351a2b1799ec2187
Felipe Santos <hydfelipe@gmail.com>
2025-12-24 12:24:08 +0100

    Añadiendo SBOM de backend

--- resource-server/src/main/java/com/aghairsalon/resourceserver/controller/ResourceController.java
+++ resource-server/src/main/java/com/aghairsalon/resourceserver/controller/ResourceController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.aghairsalon.resourceserver.model.PruebaRsrc;
 
 import java.util.List;
 
